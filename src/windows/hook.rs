f1b08044096521fb3c16e09ebed0a951290fd08a
楪蘭楓 <RanKaeder@gmail.com>
2026-09-12 08:43:15 +0800

    适配共存版懒加载初始化
    (cherry picked from commit 0f623127788f0db9882594ca3f33d1b3aff421f7)

--- src/windows/hook.rs
+++ src/windows/hook.rs
@@ -1,12 +1,18 @@
 #![allow(non_snake_case)]
 
-use std::path::{Path, PathBuf};
+use std::{path::{Path, PathBuf}, sync::atomic::{AtomicBool, Ordering}};
 
 use windows::{core::{w, PCWSTR}, Win32::{Foundation::HMODULE, System::LibraryLoader::GetModuleHandleW}};
 
 use crate::{core::{Error, Hachimi}, windows::{main::DLL_HMODULE, steamworks, utils::{self, get_module_file_name}}};
 
 use super::{hachimi_impl, proxy, ffi};
+
+static IS_LATE_LOADING: AtomicBool = AtomicBool::new(false);
+
+pub fn is_late_loading() -> bool {
+    IS_LATE_LOADING.load(Ordering::Relaxed)
+}
 
 type LoadLibraryWFn = extern "C" fn(filename: PCWSTR) -> HMODULE;
 extern "C" fn LoadLibraryW(filename: PCWSTR) -> HMODULE {
@@ -44,28 +50,39 @@ extern "C" fn LoadLibraryW(filename: PCWSTR) -> HMODULE {
 fn init_internal() -> Result<(), Error> {
     let hachimi = Hachimi::instance();
 
+    let is_late_loading = unsafe { GetModuleHandleW(w!("GameAssembly.dll")) }.is_ok();
+    if is_late_loading {
+        info!("Late loading detected");
+        IS_LATE_LOADING.store(true, Ordering::Relaxed);
+    }
+
     let module_name = PathBuf::from(unsafe { get_module_file_name(DLL_HMODULE) }.to_string())
         .file_name().map(|s| s.to_string_lossy().to_ascii_lowercase());
 
-    match module_name.as_deref() {
-        Some("unityplayer.dll") => {
-            info!("Init UnityPlayer.dll proxy");
-            proxy::unityplayer::init();
-        }
-        Some("winhttp.dll") => {
-            info!("Init winhttp.dll proxy");
-            proxy::winhttp::init(&utils::_get_system_directory());
-        }
-        other => {
-            info!("Unknown module name {:?}, skip init proxy", other);
+    if !is_late_loading {
+        match module_name.as_deref() {
+            Some("unityplayer.dll") => {
+                info!("Init UnityPlayer.dll proxy");
+                proxy::unityplayer::init();
+            }
+            Some("winhttp.dll") => {
+                info!("Init winhttp.dll proxy");
+                proxy::winhttp::init(&utils::_get_system_directory());
+            }
+            other => {
+                info!("Unknown module name {:?}, skip init proxy", other);
+            }
         }
     }
 
-    info!("Hooking LoadLibraryW");
-    hachimi.interceptor.hook(ffi::LoadLibraryW as *const () as usize, LoadLibraryW as *const () as usize)?;
+    if !is_late_loading {
+        info!("Hooking LoadLibraryW");
+        hachimi.interceptor.hook(ffi::LoadLibraryW as *const () as usize, LoadLibraryW as *const () as usize)?;
+    } else {
+        info!("Skipping LoadLibraryW hook (late loading mode)");
+    }
 
     if let Ok(handle) = unsafe { GetModuleHandleW(w!("GameAssembly.dll")) } {
-        info!("Late loading detected");
         hachimi.on_dlopen("GameAssembly.dll", handle.0 as _);
         hachimi.on_hooking_finished();   
     }
